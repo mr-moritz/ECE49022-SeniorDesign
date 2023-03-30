e561a9492d5559c676ffb45a1ad0816c88f2cb32
William Jorge <wjorgebe@purdue.edu>
2023-03-30 15:53:13 -0400

    i2c py

--- Cary_Microcomputer/i2c.py
+++ Cary_Microcomputer/i2c.py
@@ -5,7 +5,7 @@ DEVICE_ADDR = 0x15
 bus = SMBus(DEVICE_BUS)
 
 # Writing data to the I2C bus
-bus.write_byte_data(DEVICE_ADDR, 0x00, 0x01)
+bus.write_byte_data(DEVICE_ADDR, 0x01, 0x01)
 
 # Reading data from the I2C bus
-bus.read_byte_data(DEVICE_ADDR, 0x00, 2)
+# bus.read_byte_data(DEVICE_ADDR, 0x00, 2)
