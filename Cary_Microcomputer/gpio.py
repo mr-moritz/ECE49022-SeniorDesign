45e3b8831ad6eb9e12043f2356ce4d22de64d9b5
William Jorge <wjorgebe@purdue.edu>
2023-04-11 17:17:04 -0400

    Completed progress with UART and GPIO

---
+++ Cary_Microcomputer/gpio.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import serial
+import time
+import sys
+
+ser = serial.Serial('/dev/serial0', baudrate=115200 , parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=5)
+
+# Sending data to the device
+data = bytes([0x02, 0x01])
+bytes_written = ser.write(data)
+
+if bytes_written == len(data):
+	print('Data successfully written to serial port!')
+else:
+	print('Error while writing data to serial port.')
+	pass
+
+ser.close()
+time.sleep(5)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
+
+input_value = GPIO.input(4)
+print("Input value: ", input_value)
+
+GPIO.cleanup()
--- Cary_Microcomputer/uart.py
+++ Cary_Microcomputer/uart.py
@@ -1,20 +1,38 @@
 import serial
 import time
 import sys
+import RPi.GPIO as GPIO
 
-ser = serial.Serial('/dev/serial0', baudrate=115200 , parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=2)
 
+ser = serial.Serial('/dev/serial0', baudrate=9600 , parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=5, stopbits=serial.STOPBITS_ONE)
 
-#def unlock_cary():
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
+
+def unlock_cary():
 	# Sending data to the device
-	# data = bytes([0x02, 0x01])
-	# bytes_written = ser.write(data)
+	data = bytes([0x01, 0x02])
+	print(data)
+	time.sleep(1)
+	ser.flushInput()
+	ser.flushOutput()
+	bytes_written = ser.write(data)
 	
-	# if bytes_written == len(data):
-		# print('Data successfully written to serial port!')
-	# else:
-		# print('Error while writing data to serial port.')
-		# pass
+	if bytes_written == len(data):
+		print('Data successfully written to serial port!')
+	else:
+		print('Error while writing data to serial port.')
+		pass
+	
+	time.sleep(0.25)
+	
+	while GPIO.input(4) == 1:
+		print("Waiting for process to finish")
+		time.sleep(1)
+		
+	print("Process complete")
+	GPIO.cleanup()
+	
 	
 	# response = ser.readlines()
 	# print(response)
@@ -31,31 +49,35 @@ ser = serial.Serial('/dev/serial0', baudrate=115200 , parity=serial.PARITY_NONE,
 	
 	
 
-#if __name__ == "__main__":
-#	if sys.argv[1] == '-u':
-#		unlock_cary()
-#	elif sys.argv[1] == '-s':
-#		trigger_fingerprint()
+if __name__ == "__main__":
+	if sys.argv[1] == '-u':
+		unlock_cary()
+	elif sys.argv[1] == '-s':
+		trigger_fingerprint()
 		
-try:
-	ser.open()
-	print('Reading again')
-	ser.flushInput()
-	ser.flushOutput()
-	while(True):
-		response = ser.readline()
-		print(response)
-		# if response:
-			# # response = response.decode('utf-8')
-			# print(response)
-			# # ser.close()
-			# continue
-		# else:
-			# continue
-except KeyboardInterrupt:
-	ser.close()
-	print("task ended")
+# try:
+	# print('Reading again')
+	# ser.flushInput()
+	# ser.flushOutput()
+	# response = ser.read_until(expected=b'ff')
+	# print(response)
+	# if response:
+		# print("found")
+	# else:
+		# print("not found")
+	# # while response.endswith(expected_response):
+		# # data = ser.read()
+		# # print(data)
+		# # if data:
+			# # response += data.encode('utf-8')
+		# # else:
+			# # continue
+	# ser.close()
+	# # print("response matches")
+# except KeyboardInterrupt:
+	# ser.close()
+	# print("task ended")
 	
-finally:
-	ser.close()
-	print('task ended')
+# finally:
+	# ser.close()
+	# print('task ended')
